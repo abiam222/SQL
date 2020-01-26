d082418d1bab0be8d366542ee2ab9ac68d2dafcb
Abiam Velazquez <abiam222@gmail.com>
2020-01-25 23:28:02 -0500

    adding number 25 & 26 from problem set

--- SQL_Extreme/sql_queries_10-20.sql
+++ SQL_Extreme/sql_queries_10-20.sql
@@ -109,3 +109,10 @@
 -- GROUP BY ShipCountry
 -- ORDER BY AverageFreight DESC
 
+--26
+-- SELECT TOP 3 ShipCountry, AVG(Freight) AS AverageFreight 
+-- FROM Orders
+-- WHERE YEAR(OrderDate) = 2015
+-- GROUP BY ShipCountry
+-- ORDER BY AverageFreight DESC
+
